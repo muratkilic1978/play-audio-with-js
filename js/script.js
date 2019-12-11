02d1c4d7df5cbdf2b1f71fbe8331701396c94c56
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-11 13:38:23 +0100

    script.js and index.html was updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const button = document.querySelector("button");
-const bark = new Audio('mp3/bark.mp3');
+const animals = document.querySelectorAll(".animal");
+console.log(animals);
 
-button.addEventListener('click', function(){
-    let duration = bark.duration;
-    let muted = bark.muted;
-    bark.play();
-    console.log(muted);
-})
+for(let i=0; i<animals.length;i++){
+    animals[i].addEventListener("click", function(){
+        let animal = this.innerHTML;
+        console.log(animal);
+    })
+}
