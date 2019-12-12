96319942cb81c8b8ef0e2bb07ba2b722333ea71d
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-12 06:48:32 +0100

    code inside script.js was made shorter and index.html was updated

--- js/script.js
+++ js/script.js
@@ -5,34 +5,18 @@ for(let i=0; i<animals.length;i++){
     animals[i].addEventListener("click", function(){
         let animal = this.innerHTML;
         let lowerAnimal = animal.toLowerCase();
-        makeSound(lowerAnimal);
-        addStyle(lowerAnimal);
+        playit(lowerAnimal);
     })
 }
 
-function makeSound(name){
-    console.log(name);
-    switch(name){
-        case "lion": 
-            let lionSound = new Audio("mp3/lion.mp3");
-            lionSound.play();
-            break;
-        case "cougar":
-            let cougarSound = new Audio("mp3/cougar.mp3");
-            cougarSound.play();
-            break;
-        case "dog":
-            let dogSound = new Audio("mp3/bark.mp3");
-            dogSound.play();
-            break;
-    }
-}
 
-function addStyle(name){
+function playit(name){
     let activeElem = document.querySelector("."+name);
-    console.log(activeElem);
+    let Sound = new Audio("mp3/"+name+".mp3");
+    Sound.play();
     activeElem.classList.add("active");
     setTimeout(function(){
         activeElem.classList.remove("active");
     }, 300)
 }
+
